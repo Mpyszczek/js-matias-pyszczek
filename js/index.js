7d2c62b1383cd793d67ebb9e1dd38e1adad7be5d
Mpyszczek <pyszczekmatias@gmail.com>
2022-07-28 21:07:00 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+for(let i=1;i<=100;i++){
+    if( i % 2 === 0){
+        console.log('es numero par el', i);
+    }else{
+        console.log('Es numero impar el',i);
+}
+   }
+         
+                      
